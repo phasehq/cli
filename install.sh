56cae82244533e49f3c6c5ba8c016f2bf0a07c5d
Nimish <nimish@phase.dev>
2023-08-23 14:07:47 +0530

    fixed: alpine install

--- install.sh
+++ install.sh
@@ -89,49 +89,30 @@ install_package() {
             HASH_URL="$PACKAGE_URL.sha512"
             ;;
         alpine)
+            PACKAGE_PATH="$TMPDIR/phase_cli_amd64_latest.apk"
+            HASH_PATH="$TMPDIR/hash"
             PACKAGE_URL="$PACKAGE_MIRROR/latest/phase_cli_amd64_latest.apk"
             HASH_URL="$PACKAGE_URL.sha512"
+            
+            echo "Downloading phase-cli..."
+            wget_download $PACKAGE_URL $PACKAGE_PATH
+            echo "Getting phase-cli hash..."
+            wget_download $HASH_URL $HASH_PATH
+
+            EXPECTED_HASH=$(cut -d ' ' -f 1 $HASH_PATH)
+
+            if ! echo "$EXPECTED_HASH $PACKAGE_PATH" | sha512sum -c -w 2>/dev/null; then
+                echo "SHA-512 hash verification failed!"
+                exit 1
+            else
+                echo "SHA-512 hash verification successful."
+            fi
+            
+            sudo apk add --allow-untrusted $PACKAGE_PATH
             ;;
         *)
             install_binary
             exit 0
-            ;;
-    esac
-
-    PACKAGE="$TMPDIR/package"
-    HASH="$TMPDIR/hash"
-
-    echo "Downloading phase-cli..."
-    wget_download $PACKAGE_URL $PACKAGE
-    echo "Getting phase-cli hash..."
-    wget_download $HASH_URL $HASH
-
-    EXPECTED_HASH=$(cut -d ' ' -f 1 $HASH)
-
-    if ! echo "$EXPECTED_HASH $PACKAGE" | sha512sum -c -w 2>/dev/null; then
-        echo "SHA-512 hash verification failed!"
-        exit 1
-    else
-        echo "SHA-512 hash verification successful."
-    fi
-
-    if ! has_sudo_access; then
-        echo "Installing phase-cli..."
-        echo "Please enter your sudo password."
-    fi
-    case $OS in
-        ubuntu|debian)
-            sudo dpkg -i $PACKAGE
-            ;;
-        fedora|rhel|centos)
-            if is_phase_installed; then
-                sudo rpm -U $PACKAGE
-            else
-                sudo rpm -i $PACKAGE
-            fi
-            ;;
-        alpine)
-            sudo apk add --allow-untrusted $PACKAGE
             ;;
     esac
 
