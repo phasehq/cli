37bb56757cb91af88392525e21e3a571e925615e
Nimish <85357445+nimish-ks@users.noreply.github.com>
2023-08-23 14:18:08 +0530

    Install script fixes (#41)
    * fixed: alpine install

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ check_required_tools() {
             echo "Installing $TOOL..."
             case $OS in
                 ubuntu|debian)
-                    apt-get update && sudo apt-get install -y $TOOL
+                    apt-get update && apt-get install -y $TOOL
                     ;;
                 fedora|rhel|centos)
                     yum install -y $TOOL
@@ -50,7 +50,6 @@ is_phase_installed() {
 }
 
 wget_download() {
-    # Check if wget supports --show-progress
     if wget --help 2>&1 | grep -q '\--show-progress'; then
         wget --show-progress "$1" -O "$2"
     else
@@ -81,62 +80,85 @@ install_package() {
 
     case $OS in
         ubuntu|debian)
+            PACKAGE_PATH="$TMPDIR/phase_cli_amd64_latest.deb"
+            HASH_PATH="$TMPDIR/hash"
             PACKAGE_URL="$PACKAGE_MIRROR/latest/phase_cli_amd64_latest.deb"
             HASH_URL="$PACKAGE_URL.sha512"
+            
+            echo "Downloading phase-cli..."
+            wget_download $PACKAGE_URL $PACKAGE_PATH
+            echo "Getting phase-cli hash..."
+            wget_download $HASH_URL $HASH_PATH
+
+            EXPECTED_HASH=$(cut -d ' ' -f 1 $HASH_PATH)
+
+            if ! echo "$EXPECTED_HASH $PACKAGE_PATH" | sha512sum -c -w 2>/dev/null; then
+                echo "SHA-512 hash verification failed!"
+                exit 1
+            else
+                echo "SHA-512 hash verification successful."
+            fi
+            
+            sudo dpkg -i $PACKAGE_PATH
             ;;
+
         fedora|rhel|centos)
+            PACKAGE_PATH="$TMPDIR/phase_cli_amd64_latest.rpm"
+            HASH_PATH="$TMPDIR/hash"
             PACKAGE_URL="$PACKAGE_MIRROR/latest/phase_cli_amd64_latest.rpm"
             HASH_URL="$PACKAGE_URL.sha512"
+            
+            echo "Downloading phase-cli..."
+            wget_download $PACKAGE_URL $PACKAGE_PATH
+            echo "Getting phase-cli hash..."
+            wget_download $HASH_URL $HASH_PATH
+
+            EXPECTED_HASH=$(cut -d ' ' -f 1 $HASH_PATH)
+
+            if ! echo "$EXPECTED_HASH $PACKAGE_PATH" | sha512sum -c -w 2>/dev/null; then
+                echo "SHA-512 hash verification failed!"
+                exit 1
+            else
+                echo "SHA-512 hash verification successful."
+            fi
+
+            if is_phase_installed; then
+                sudo rpm -U $PACKAGE_PATH
+            else
+                sudo rpm -i $PACKAGE_PATH
+            fi
             ;;
+
         alpine)
+            PACKAGE_PATH="$TMPDIR/phase_cli_amd64_latest.apk"
+            HASH_PATH="$TMPDIR/hash"
             PACKAGE_URL="$PACKAGE_MIRROR/latest/phase_cli_amd64_latest.apk"
             HASH_URL="$PACKAGE_URL.sha512"
+            
+            echo "Downloading phase-cli..."
+            wget_download $PACKAGE_URL $PACKAGE_PATH
+            echo "Getting phase-cli hash..."
+            wget_download $HASH_URL $HASH_PATH
+
+            EXPECTED_HASH=$(cut -d ' ' -f 1 $HASH_PATH)
+
+            if ! echo "$EXPECTED_HASH $PACKAGE_PATH" | sha512sum -c -w 2>/dev/null; then
+                echo "SHA-512 hash verification failed!"
+                exit 1
+            else
+                echo "SHA-512 hash verification successful."
+            fi
+            
+            sudo apk add --allow-untrusted $PACKAGE_PATH
             ;;
+
         *)
             install_binary
             exit 0
             ;;
     esac
 
-    PACKAGE="$TMPDIR/package"
-    HASH="$TMPDIR/hash"
-
-    echo "Downloading phase-cli..."
-    wget_download $PACKAGE_URL $PACKAGE
-    echo "Getting phase-cli hash..."
-    wget_download $HASH_URL $HASH
-
-    EXPECTED_HASH=$(cut -d ' ' -f 1 $HASH)
-
-    if ! echo "$EXPECTED_HASH $PACKAGE" | sha512sum -c -w 2>/dev/null; then
-        echo "SHA-512 hash verification failed!"
-        exit 1
-    else
-        echo "SHA-512 hash verification successful."
-    fi
-
-    if ! has_sudo_access; then
-        echo "Installing phase-cli..."
-        echo "Please enter your sudo password."
-    fi
-    case $OS in
-        ubuntu|debian)
-            sudo dpkg -i $PACKAGE
-            ;;
-        fedora|rhel|centos)
-            if is_phase_installed; then
-                sudo rpm -U $PACKAGE
-            else
-                sudo rpm -i $PACKAGE
-            fi
-            ;;
-        alpine)
-            sudo apk add --allow-untrusted $PACKAGE
-            ;;
-    esac
-
-    # Fetch the version using phase -v
-    VERSION=$(phase -v)
+    VERSION=$(phase -v 2>/dev/null || echo "Unknown")
     echo "phase-cli version $VERSION successfully installed"
 }
 
