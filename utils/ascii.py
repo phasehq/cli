d9a20a3612210ee5c1bb5e2dd3329fff74e99e4f
Nimish <nimish@phase.dev>
2023-09-01 14:57:54 +0530

    chore: cleaned up phase ascii logo

--- utils/ascii.py
+++ utils/ascii.py
@@ -1,17 +1,1 @@
 import datetime
-
-phaseASCii = f"""
-         :@tX88%%:                   
-        ;X;%;@%8X@;    Phase-cli               
-      ;Xt%;S8:;;t%S    Encrypt the signal, store the noise.
-      ;SXStS@.;t8@:;.  
-    ;@:t;S8  ;@.%.;8:  © {datetime.datetime.now().year} Phase Security Inc.
-    :X:S%88    S.88t:. https://phase.dev
-  :X:%%88     :S:t.t8t
-.@8X888@88888888X8.%8X8888888X8.S88: 
-                ;t;X8;      ;XS:%X;
-                :@:8@X.     XXS%S8    
-                 8XX:@8S  .X%88X;
-                  ..@:X88:8Xt8:     
-                     ;%88@S8:                  
-    """
