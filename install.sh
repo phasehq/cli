21c4fed7c044bcabc359bd2c8f64c42fa96f48fb
Nimish <nimish@phase.dev>
2023-09-05 13:27:44 +0530

    feat: added sha256 verification

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ detect_os() {
 }
 
 check_required_tools() {
-    for TOOL in sudo wget curl jq; do
+    for TOOL in sudo wget curl jq sha256sum; do
         if ! command -v $TOOL > /dev/null; then
             echo "Installing $TOOL..."
             case $OS in
@@ -37,7 +37,6 @@ check_required_tools() {
     done
 }
 
-
 get_latest_version() {
     curl -s "https://api.github.com/repos/$REPO/releases/latest" | jq -r .tag_name | cut -c 2- # remove the 'v' prefix
 }
@@ -47,6 +46,20 @@ wget_download() {
         wget --show-progress "$1" -O "$2"
     else
         wget "$1" -O "$2"
+    fi
+}
+
+verify_checksum() {
+    local file="$1"
+    local checksum_url="$2"
+    local checksum_file="$TMPDIR/checksum.sha256"
+    
+    wget_download "$checksum_url" "$checksum_file"
+    sha256sum -c "$checksum_file" --ignore-missing
+
+    if [[ $? -ne 0 ]]; then
+        echo "Checksum verification failed!"
+        exit 1
     fi
 }
 
@@ -66,7 +79,20 @@ is_phase_installed() {
     fi
 }
 
+is_phase_installed_via_pip() {
+    if pip list 2>/dev/null | grep -q phase-cli || pip3 list 2>/dev/null | grep -q phase-cli; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 install_package() {
+    if is_phase_installed_via_pip; then
+        echo "phase-cli is already installed via pip. Please upgrade using pip."
+        exit 0
+    fi
+
     VERSION=$(get_latest_version)
     TMPDIR=$(mktemp -d)
 
@@ -74,12 +100,14 @@ install_package() {
         ubuntu|debian)
             PACKAGE_URL="$BASE_URL/v$VERSION/phase_cli_linux_amd64_$VERSION.deb"
             wget_download $PACKAGE_URL $TMPDIR/phase.deb
+            verify_checksum "$TMPDIR/phase.deb" "$PACKAGE_URL.sha256"
             sudo dpkg -i $TMPDIR/phase.deb
             ;;
 
         fedora|rhel|centos)
             PACKAGE_URL="$BASE_URL/v$VERSION/phase_cli_linux_amd64_$VERSION.rpm"
             wget_download $PACKAGE_URL $TMPDIR/phase.rpm
+            verify_checksum "$TMPDIR/phase.rpm" "$PACKAGE_URL.sha256"
             if is_phase_installed; then
                 sudo rpm -U $TMPDIR/phase.rpm
             else
@@ -88,18 +116,26 @@ install_package() {
             ;;
 
         alpine)
-            BINARY_URL="$BASE_URL/v$VERSION/phase_cli_alpine_linux_amd64_$VERSION"
-            wget_download $BINARY_URL $TMPDIR/phase
-            chmod +x $TMPDIR/phase
-            if ! has_sudo_access; then
-                echo "Moving binary to /usr/local/bin. Please enter your sudo password or run as root."
-            fi
-            sudo mv $TMPDIR/phase /usr/local/bin/
+            PACKAGE_URL="$BASE_URL/v$VERSION/phase_cli_alpine_linux_amd64_$VERSION.apk"
+            wget_download $PACKAGE_URL $TMPDIR/phase.apk
+            verify_checksum "$TMPDIR/phase.apk" "$PACKAGE_URL.sha256"
+            sudo apk add --allow-untrusted $TMPDIR/phase.apk
             ;;
 
         *)
-            echo "Unsupported OS type."
-            exit 1
+            if [ "$(uname -m)" == "x86_64" ]; then
+                BINARY_URL="$BASE_URL/v$VERSION/phase_cli_linux_amd64_$VERSION"
+                wget_download $BINARY_URL $TMPDIR/phase
+                verify_checksum "$TMPDIR/phase" "$BINARY_URL.sha256"
+                chmod +x $TMPDIR/phase
+                if ! has_sudo_access; then
+                    echo "Moving binary to /usr/local/bin. Please enter your sudo password or run as root."
+                fi
+                sudo mv $TMPDIR/phase /usr/local/bin/
+            else
+                echo "Unsupported OS type and architecture."
+                exit 1
+            fi
             ;;
     esac
 
