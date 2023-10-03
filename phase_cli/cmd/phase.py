780bfca716dea9de0f0c13c35dda1bdb9319b42e
Nimish <nimish@phase.dev>
2023-10-03 13:31:45 +0530

    feat: added support for stdin secret creation

--- phase_cli/cmd/phase.py
+++ phase_cli/cmd/phase.py
@@ -159,20 +159,25 @@ def phase_secrets_create(key=None, env_name=None, phase_app=None):
         env_name (str, optional): The name of the environment where the secret will be created. Defaults to None.
         phase_app (str, optional): The name of the Phase application. Defaults to None.
     """
+
     # Initialize the Phase class
     phase = Phase()
-    
+
     # If the key is not passed as an argument, prompt user for input
     if key is None:
         key = input("🗝️  Please enter the key: ")
     key = key.upper()
-        
-    value = getpass.getpass("✨ Please enter the value (hidden): ")
-    
+
+    # Check if input is being piped
+    if sys.stdin.isatty():
+        value = getpass.getpass("✨ Please enter the value (hidden): ")
+    else:
+        value = sys.stdin.read().strip()
+
     try:
         # Encrypt and send secret to the backend using the `create` method
         response = phase.create(key_value_pairs=[(key, value)], env_name=env_name, app_name=phase_app)
-        
+
         # Check the response status code
         if response.status_code == 200:
             # Call the phase_list_secrets function to list the secrets
@@ -180,7 +185,7 @@ def phase_secrets_create(key=None, env_name=None, phase_app=None):
         else:
             # Print an error message if the response status code indicates an error
             print(f"Error: Failed to create secret. HTTP Status Code: {response.status_code}")
-            
+
     except ValueError:
         print(f"⚠️  Warning: The environment '{env_name}' either does not exist or you do not have access to it.")
 
@@ -455,8 +460,6 @@ def phase_list_secrets(show=False, env_name=None, phase_app=None):
 
     except ValueError as e:
         print(f"⚠️  Warning: The environment '{env_name}' either does not exist or you do not have access to it.")
-
-
 
 
 def phase_run_inject(command, env_name=None, phase_app=None):
