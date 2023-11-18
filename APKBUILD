938a09e735ca211d4f7b34ce65f4c89b8d0b0922
Nimish <nimish@phase.dev>
2023-11-18 19:01:43 +0530

    chore: bump version

--- APKBUILD
+++ APKBUILD
@@ -1,6 +1,6 @@
 # Maintainer: Phase <info@phase.dev>
 pkgname=phase
-pkgver=1.8.1
+pkgver=1.9.0
 pkgrel=0
 pkgdesc="Phase CLI"
 url="https://phase.dev"
--- phase_cli/utils/const.py
+++ phase_cli/utils/const.py
@@ -1,6 +1,6 @@
 import os
 import re
-__version__ = "1.8.1"
+__version__ = "1.9.0"
 __ph_version__ = "v1"
 
 description = "Securely manage and sync environment variables with Phase."
