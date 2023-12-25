6e113014db63fe42a190dc9e4b11e5caefa2203d
Nimish <nimish@phase.dev>
2023-12-25 18:19:24 +0530

    chore: beautified secrets dict in tests

--- tests/secrets_export.py
+++ tests/secrets_export.py
@@ -12,9 +12,20 @@ from phase_cli.cmd.secrets.export import (
     export_dotenv, export_hcl, export_ini, export_java_properties
 )
 
-secrets_dict = {'AWS_SECRET_ACCESS_KEY': 'aCRAMarEbFC3Q5c24pi7AVMIt6TaCfHeFZ4KCf/a', 'AWS_ACCESS_KEY_ID': 'AKIAIX4ONRSG6ODEFVJA', 'JWT_SECRET': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjMzNjIwMTcxLCJleHAiOjIyMDg5ODUyMDB9.pHnckabbMbwTHAJOkb5Z7G7B4chY6GllJf6K2m96z3A', 'STRIPE_SECRET_KEY': 'sk_test_EeHnL644i6zo4Iyq4v1KdV9H', 'DJANGO_SECRET_KEY': 'wwf*2#86t64!fgh6yav$aoeuo@u2o@fy&*gg76q!&%6x_wbduad', 'DJANGO_DEBUG': 'True', 'POSTGRES_CONNECTION_STRING': 'postgresql://postgres:6c37810ec6e74ec3228416d2844564fceb99ebd94b29f4334c244db011630b0e@mc-laren-prod-db.c9ufzjtplsaq.us-west-1.rds.amazonaws.com:5432/XP1_LM', 'DB_HOST': 'mc-laren-prod-db.c9ufzjtplsaq.us-west-1.rds.amazonaws.com', 'DB_USER': 'postgres', 'DB_NAME': 'XP1_LM', 'DB_PASSWORD': '6c37810ec6e74ec3228416d2844564fceb99ebd94b29f4334c244db011630b0e', 'DB_PORT': '5432'}
-
-
+secrets_dict = {
+    'AWS_SECRET_ACCESS_KEY': 'aCRAMarEbFC3Q5c24pi7AVMIt6TaCfHeFZ4KCf/a',
+    'AWS_ACCESS_KEY_ID': 'AKIAIX4ONRSG6ODEFVJA',
+    'JWT_SECRET': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjMzNjIwMTcxLCJleHAiOjIyMDg5ODUyMDB9.pHnckabbMbwTHAJOkb5Z7G7B4chY6GllJf6K2m96z3A',
+    'STRIPE_SECRET_KEY': 'sk_test_EeHnL644i6zo4Iyq4v1KdV9H', 
+    'DJANGO_SECRET_KEY': 'wwf*2#86t64!fgh6yav$aoeuo@u2o@fy&*gg76q!&%6x_wbduad',
+    'DJANGO_DEBUG': 'True', 
+    'POSTGRES_CONNECTION_STRING': 'postgresql://postgres:6c37810ec6e74ec3228416d2844564fceb99ebd94b29f4334c244db011630b0e@mc-laren-prod-db.c9ufzjtplsaq.us-west-1.rds.amazonaws.com:5432/XP1_LM', 
+    'DB_HOST': 'mc-laren-prod-db.c9ufzjtplsaq.us-west-1.rds.amazonaws.com', 
+    'DB_USER': 'postgres', 
+    'DB_NAME': 'XP1_LM', 
+    'DB_PASSWORD': '6c37810ec6e74ec3228416d2844564fceb99ebd94b29f4334c244db011630b0e', 
+    'DB_PORT': '5432'
+}
 
 def test_export_json(capsys):
     export_json(secrets_dict)
@@ -60,7 +71,7 @@ def test_export_toml(capsys):
     captured = capsys.readouterr()
     assert toml.loads(captured.out) == secrets_dict
 
-
+# TODO: Validate HCL output
 # def test_export_hcl(capsys):
 #     export_hcl(secrets_dict)
 #     captured = capsys.readouterr()
