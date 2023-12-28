96a5af249ac72988b07e23da0f9a33bfedd7ab6b
Nimish <nimish@phase.dev>
2023-12-28 15:09:07 +0530

    fix: local_ref_pattern regex

--- phase_cli/utils/const.py
+++ phase_cli/utils/const.py
@@ -35,4 +35,5 @@ pss_user_pattern = re.compile(r"^pss_user:v(\d+):([a-fA-F0-9]{64}):([a-fA-F0-9]{
 pss_service_pattern = re.compile(r"^pss_service:v(\d+):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64})$")
 
 cross_env_pattern = re.compile(r"\$\{(.+?)\.(.+?)\}")
-local_ref_pattern = re.compile(r"\$\{(.+?)\}")
+local_ref_pattern = re.compile(r"\$\{([^.]+?)\}")
+
