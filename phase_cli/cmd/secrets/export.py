90415ad09e18fe6a5b1aaff5e44eabee167b575d
Nimish <nimish@phase.dev>
2023-12-28 12:53:53 +0530

    wip: fix cross env

--- phase_cli/cmd/secrets/export.py
+++ phase_cli/cmd/secrets/export.py
@@ -84,34 +84,36 @@ def phase_secrets_env_export(env_name=None, phase_app=None, keys=None, tags=None
 def resolve_references(secrets_dict, phase, env_name, phase_app):
     """
     Resolve references in secret values.
-
-    Args:
-        secrets_dict (dict): Dictionary of secrets.
-        phase (Phase): Phase instance.
-        env_name (str): Environment name.
-        phase_app (str): Phase application name.
-
-    Returns:
-        dict: Updated secrets dictionary with resolved references.
     """
     for key, value in secrets_dict.items():
-        # Resolve cross environment references
+        # Track found references to avoid duplicate warnings
+        found_references = set()
+
+        # Resolve cross-environment references
         cross_env_matches = re.findall(cross_env_pattern, value)
         for ref_env, ref_key in cross_env_matches:
+            full_ref = f"{ref_env}.{ref_key}"
+            found_references.add(full_ref)
+
             try:
-                ref_secret = phase.get(env_name=ref_env, keys=[ref_key], app_name=phase_app)[0]
-                value = value.replace(f"${{{ref_env}.{ref_key}}}", ref_secret['value'])
-            except ValueError:
-                print(f"# Warning: Issue with {ref_env} for key {key}. Reference {ref_key} not found.")
+                ref_secrets = phase.get(env_name=ref_env, keys=[ref_key], app_name=phase_app)
+                if ref_secrets:
+                    ref_secret = ref_secrets[0]
+                    value = value.replace(f"${{{full_ref}}}", ref_secret['value'])
+                else:
+                    print(f"# Warning: Secret '{ref_key}' not found in environment '{ref_env}' for key '{key}'.")
+            except ValueError as e:
+                print(f"# Error: Issue with fetching secret '{ref_key}' from environment '{ref_env}' for key '{key}'. {e}")
 
         # Resolve local references
         local_ref_matches = re.findall(local_ref_pattern, value)
         for ref_key in local_ref_matches:
-            if ref_key in secrets_dict:
-                ref_value = secrets_dict[ref_key]
-                value = value.replace(f"${{{ref_key}}}", ref_value)
-            else:
-                print(f"# Warning: Local reference {ref_key} not found for key {key}.")
+            if ref_key not in found_references:
+                if ref_key in secrets_dict:
+                    ref_value = secrets_dict[ref_key]
+                    value = value.replace(f"${{{ref_key}}}", ref_value)
+                else:
+                    print(f"# Warning: Local reference '{ref_key}' not found for key '{key}'.")
 
         secrets_dict[key] = value
 
