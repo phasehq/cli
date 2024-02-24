631386e747780f17812759681ace7047fccf20af
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-02-24 20:33:16 +0530

    feat: add custom exception class for EnvironmentNotFound

---
+++ phase_cli/exceptions.py
@@ -0,0 +1,3 @@
+class EnvironmentNotFoundException(Exception):
+    def __init__(self, env_name):
+        super().__init__(f"⚠️\u200A Warning: The environment '{env_name}' either does not exist or you do not have access to it.")
