c4e81971aac49fc0aa2eefa78a82cef623c444f8
Nimish <nimish@phase.dev>
2024-02-28 11:39:50 +0530

    chore: bumped versions

--- phase_cli/utils/const.py
+++ phase_cli/utils/const.py
@@ -1,6 +1,7 @@
 import os
 import re
-__version__ = "1.12.0"
+
+__version__ = "1.13.0"
 __ph_version__ = "v1"
 
 description = "Securely manage application secrets and environment variables with Phase."
