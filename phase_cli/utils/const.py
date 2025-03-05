aeceffd19b017d4a6d6d2e9ba6e7d31ed9b6646a
Nimish <85357445+nimish-ks@users.noreply.github.com>
2025-03-05 21:27:26 +0530

    chore: bump version to v.1.19.0 (#197)

--- phase_cli/utils/const.py
+++ phase_cli/utils/const.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-__version__ = "1.18.7"
+__version__ = "1.19.0"
 __ph_version__ = "v1"
 
 description = "Securely manage application secrets and environment variables with Phase."
