babd92f3f4f48cb3cfd486581ad3f7d13fd818f3
Nimish <nimish@phase.dev>
2025-09-29 18:00:41 +0530

    feat: update phase_secrets_get function to include generate_leases and lease_ttl parameters for enhanced dynamic secret retrieval

--- phase_cli/cmd/secrets/get.py
+++ phase_cli/cmd/secrets/get.py
@@ -3,7 +3,7 @@ from phase_cli.utils.phase_io import Phase
 from rich.console import Console
 import json
 
-def phase_secrets_get(key, env_name=None, phase_app=None, phase_app_id=None, tags=None, path='/'):
+def phase_secrets_get(key, env_name=None, phase_app=None, phase_app_id=None, tags=None, path='/', generate_leases: str = 'true', lease_ttl: int = None):
     """
     Fetch and print a single secret based on a given key as beautified JSON with syntax highlighting.
 
@@ -19,7 +19,8 @@ def phase_secrets_get(key, env_name=None, phase_app=None, phase_app_id=None, tag
     
     try:
         key = key.upper()
-        secrets_data = phase.get(env_name=env_name, keys=[key], app_name=phase_app, app_id=phase_app_id, tag=tags, path=path)
+        lease_flag = str(generate_leases).lower() not in ['false', '0', 'no']
+        secrets_data = phase.get(env_name=env_name, keys=[key], app_name=phase_app, app_id=phase_app_id, tag=tags, path=path, dynamic=True, lease=lease_flag, lease_ttl=lease_ttl)
         
         # Find the specific secret for the given key within the provided path
         secret_data = next((secret for secret in secrets_data if secret["key"] == key), None)
