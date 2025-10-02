2bcd95bdb0ec3e7645873e69b597b9dc080ced7d
Nimish <nimish@phase.dev>
2025-10-02 19:01:56 +0530

    feat: add AWS configuration constants for STS endpoint and region
    - Introduced AWS_DEFAULT_GLOBAL_STS_ENDPOINT and AWS_DEFAULT_GLOBAL_STS_REGION constants to facilitate AWS service integration.
    - Updated PHASE_CLOUD_API_HOST for clarity in configuration management.

--- phase_cli/utils/const.py
+++ phase_cli/utils/const.py
@@ -33,8 +33,12 @@ CONFIG_FILE = os.path.join(
     PHASE_SECRETS_DIR, "config.json"
 )  # Holds local user account configurations
 
+
 PHASE_CLOUD_API_HOST = "https://console.phase.dev"
-PHASE_CLOUD_PUBLIC_API_HOST = "https://api.phase.dev"
+
+# AWS Config
+AWS_DEFAULT_GLOBAL_STS_ENDPOINT = "https://sts.amazonaws.com"
+AWS_DEFAULT_GLOBAL_STS_REGION = "us-east-1"
 
 pss_user_pattern = re.compile(
     r"^pss_user:v(\d+):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64})$"
